9e6b528d6e9d1e0cf771a910af9a535f377fae52
Marcelo Vilela <marcelo.filho.cps@gmail.com>
2018-11-22 19:20:11 +0000

    Atualizado comentários do BICO

--- bico.s
+++ bico.s
@@ -10,65 +10,63 @@
 .text
 .align 4
 
-@ set_motor_speed
-@ Parametros:
-@ r0: ponteiro para struct "motor_cfg_t" (2 bytes)
-@ Retorno:
-@ -
+@ Função set_motor_speed
+@ Parametro 1: r0 - Endereço do struct motor_cfg_t
+@ Retorno 1: Sem retorno
+@ OBS: mapeado para a syscall 20.
+@ OBS¹: o struct contém dois endereços de unsigned char(1 byte cada)
+@ o primeiro indicando o id, e o segundo indicando a velocidade
+@ OBS²: a função ldrb carrega meio byte de cada endereço - á direita
 set_motor_speed:
     push {r7, lr}
 
-    @ Parametros da syscall 20
-    ldrb r1, [r0, #1] @ carrega byte 2 da struct no r1
-    ldrb r0, [r0] @ carrega byte 1 da struct no r0
+    ldrb r1, [r0, #1] @ Carrega velocidade em r1
+    ldrb r0, [r0] @ Carrega id em r0
 
-    mov r7, #20
-    svc 0x0 @ chama a syscall 20, definida pelo enunciado
+    mov r7, #20 @ Coloca em r7 o numero da syscall(definida no enunciado)
+    svc 0x0
 
     pop {r7, pc}
 
-@ read_sonar
-@ Parametros:
-@ r0: identificador do sonar (0 a 15)
-@ Retorno:
-@ r0: distancia / -1: sonar invalido
+@ Função read_sonar
+@ Parametros 1: r0 - id do sonar a ser lido (0 a 15)
+@ Retorno 1: distancia ou sonar invalido -1
+@ OBS: mapeado para a syscall 21
 read_sonar:
     push {r7, lr}
 
-    mov r7, #21
-    svc 0x0 @ chama a syscall 21, definida pelo enunciado
+    mov r7, #21 @ Coloca em r7 o numero da syscall(definida no enunciado)
+    svc 0x0
 
     pop {r7, pc}
 
 
 
 
-@ get_time
-@ Parametros:
-@ r0: ponteiro para variavel que recebera o tempo do sistema
-@ Retorno:
-@ -
+@ Função set_time
+@ Parametros 1: r0 - valor a ser setado na velocidade
+@ Retorno 1: Sem retorno
+@ OBS: mapeado para a syscall 18
 set_time:
     push {r4, r7, lr}
 
-    mov r4, r0 @ guarda parametro da syscall 18
+    mov r4, r0 @ guarda valor setado na velocidade
 
-    mov r7, #18
-    svc 0x0 @ chama a syscall 18, definida pelo enunciado
+    mov r7, #18 @ Coloca em r7 o numero da syscall(definida no enunciado)
+    svc 0x0
 
     str r0, [r4] @ guarda o retorno no endereco do parametro
 
     pop {r4, r7, pc}
 
-@ get_time
-@ Parametros:
-@ r0: tempo do sistema
-@ Retorno:
-@ r0: tempo de sistema
+@ Função get_time
+@ Parametro 1: sem parametros
+@ Retorno 1: r0 - tempo atual do sistema
+@ OBS: mapeado para a syscall 17
 get_time:
     push {r7, lr}
 
-    mov r7, #17
-    svc 0x0 @ chama a syscall 17, definida pelo enunciado
+    mov r7, #17 @ Coloca em r7 o numero da syscall(definida no enunciado)
+    svc 0x0a
 
     pop {r7, pc}
